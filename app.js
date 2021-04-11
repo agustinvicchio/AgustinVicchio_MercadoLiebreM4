8b91d7d415eacbb6b00136b30c9acabc86305282
Agustín Mario Vicchio <agustinvicchio@fb.com>
2021-04-11 17:16:44 -0300

    Agustin subiendo nuevas direcciones en app y file heroku

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.use(express.static(publicPath));
 
 app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/index.html'));});
 
-app.listen (3000, () => { console.log('servidor corriendo')});
+app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/login.html'));});
+
+app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/registar.html'));});
+
+app.listenerCount(process.env.PORT || 3000, function() {
+    console.log ('Servidor corriendo en el puerto 3000');
+});
+
+/*app.listen (3000, () => { console.log('servidor corriendo')});*/
